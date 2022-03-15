9810b0b1a5453c45f9d75765f8e04007c1694933
TirlaP <46626173+TirlaP@users.noreply.github.com>
2022-03-15 09:12:58 +0200

    Add files via upload

---
+++ Minesweeper C#/Minesweeper/Minesweeper/Boards/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Minesweeper.Boards
+{
+    public class Board
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int NumMines { get; set; }
+        // public int NumMinesRemaining => NumMines - FlagsPlaced();
+        public bool ShowMines { get; set; }
+        public bool ShowHints { get; set; }
+        public bool ShowPercentage { get; set; }
+        public bool ShowLocation { get; set; }
+        public bool ShowCellHighlights { get; set; }
+        public bool GameOver { get; set; }
+
+        public const int CellSize = 32;
+
+    }
+}
---
+++ Minesweeper C#/Minesweeper/Minesweeper/Cells/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Minesweeper.Core.Boards;
+using Minesweeper.Core.Cells;
+
+namespace Minesweeper.Core
+{
+    public class Cell
+    {
+        public int XLoc { get; set; }
+        public int YLoc { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public int CellSize { get; set; }
+        public int NumMines { get; set; }
+
+    }
+}
